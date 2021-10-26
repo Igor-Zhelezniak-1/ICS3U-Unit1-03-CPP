7debdf048c70100fd410c6291948801cee0eff39
Igor <ihor.zhelezniak@mths.ca>
2021-10-26 23:09:41 +0000

    aded program

---
+++ program.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2021 Igor All rights reserved
+//
+// Created by: Igor
+// Created on: Oct 2021
+// This is the "Hello, World!" program, with proper style
+
+#include <iostream>
+int main() {
+  std::cout << "Hello, World!";
+}
